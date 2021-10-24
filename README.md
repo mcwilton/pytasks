944c3f6c09f32e9c817d5700870a1fb345cb9dd4
McWilton <mcwilton85@gmail.com>
2021-10-24 23:38:15 +0200

    updates

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pytasks
  pytasks
+
+To run command line use:
+
+1) python manage.py user_details
+
+To get an excel sheet with details about users
+
+2) python manage.py user_tasks
+
+To get an excel sheet with tasks in tabs
--- pytasks/settings.py
+++ pytasks/settings.py
@@ -79,10 +79,23 @@ WSGI_APPLICATION = 'pytasks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#change details tofint your own database.
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pytasks',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
