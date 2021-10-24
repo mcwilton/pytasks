bc709e1e17473da20e0c3453f09a57adb5fefc28
McWilton <mcwilton85@gmail.com>
2021-10-24 21:15:56 +0200

    update

--- pytasks/settings.py
+++ pytasks/settings.py
@@ -138,4 +138,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CRISPY_TEMPLATE_PACK =  'bootstrap4'
 
-LOGIN_REDIRECT_URL =  '/'
+LOGIN_REDIRECT_URL =  'current/'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':   
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Bravo, {username} account created. \nPlease Login')
+            return redirect('account-login')
+    else:
+        form = CreateUserForm()
+       
+    context = {
+        'form': form
+    }
+    return render(request, 'register.html', context)
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def profile_update(request):
+    if request.method == 'POST':
+        userform = UserUpdateForm(request.POST, instance=request.user)
+        profileform = UserUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+            profileform.save()
+            return redirect('account-profile')
+    else:
+        userform = UserUpdateForm(instance=request.user)
+        profileform = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'userform': userform,
+        'profileform': profileform,
+    }
+    return render(request, 'profile_update.html', context)
+
+                
+def loginPage(request):
+    return render(request, 'login.html')
+
+
